ed036e20cd6ca03ee841ce950377870a3520d025
cadenzig <38665217+cadenzig@users.noreply.github.com>
2018-05-09 20:54:45 -0700

    inital commit

--- display.js
+++ display.js
@@ -6,27 +6,57 @@ function setup(){
     var width = 400;
     var height = 400;
     game = createGameState();
-    frameRate(2);
+    frameRate(1500);
     createCanvas(width, height); 
 }
 
 function draw(){
-    background(0);
-}
+    updateGameState(game);
+    if(game.isDead)
+        background("balck")
+    else
+        background("blue")
 
-var pointSize = 10;
-var pointSpacing = 20;
+
+    drawGameState(game);
+        
+}
+// physical units
+var pointSize = 40;
+var pointSpacing = 50;
+var margin = 50;
 var colors = [
     "#3f51b5",
     "#03a9f5",
     "#ea1e63",
     "#fec107"
 ];
+// converts logical grind into physical display
 
 function drawPoint(x,y,colorIndex){
-    console.log(`drawPoint: ${keyCode}`);
+    var color = "red";
+    if (colorIndex == 1) 
+        {color = "yellow"}
+    if (colorIndex == 2)
+        [color= "pink"]
+    fill(colors[colorIndex]);
+    var jitter = random(5);
+    ellipse(x* pointSpacing + margin, y* pointSpacing + margin, pointSize+jitter);
+    
+    
 }
 
 function keyPressed() {
     console.log(`keyPressed: ${keyCode}`);
+    if (keyCode == 38) game.playerY --;
+    if (keyCode == 40) game.playerY ++;
+    if (keyCode == LEFT_ARROW) game.playerX --;
+    if (keyCode == 39) game.playerX ++;
+
+    if (game.playerX >= game.cols) game.playerX = 0;
+    if (game.playerY >= game.rows) game.playerY = 0;
+
+    if (game.playerX < 0) game.playerX = game.cols - 1;
+    if (game.playerY < 0) game.playerY = game.rows - 1;
+
 }
--- game.js
+++ game.js
@@ -1,11 +1,104 @@
 // functions
 
-function createGameState(){}
+function createGameState(){
+    var game = {};
+
+
+
+    game.score = 0;
+
+    game.isDead = false;
+    game.playerX = 2;
+    game.playerY= 2;
+    
+
+    game.coinX = 0;
+    game.coinY= 0;
+
+    var grid = [];
+    grid.push([1,1,1,1,1]);
+    grid.push([1,0,0,0,1]);
+    grid.push([1,0,0,0,1]);
+    grid.push([0,0,0,0,0]);
+    grid.push([1,0,0,0,1]);
+    grid.push([1,0,0,0,1]);
+    grid.push([1,1,1,1,1]);
+
+    game.grid = grid;
+    game.rows = grid.length;
+    game.cols = grid[0].length
+
+
+    setRandomCoin(game);
+
+    return game;
+}
 function createEmptyGrid(){}
 function createLevel(){}
-function setRandomCoin(){}
-function drawGameState(){}
-function updateGameState(){}
+function setRandomCoin(game){
+
+    var collided = true;
+
+    while(collided){
+        game.coinX = Math.floor(random(game.cols-1));
+        game.coinY = Math.floor(random(game.rows-1));
+
+        //check to see if coin hit wall
+        var x = game.coinX;
+        var y = game.coinY;
+        var hitwall =  (game.grid[y][x] == 1);
+
+        var hitplayer = (game.coinX == game.playerX && game.coinY == game.playerY);
+        collided = hitwall || hitplayer;
+        
+        
+    }
+
+
+
+}
+
+
+
+function drawGameState(game){
+    //draw the walls
+  
+    for(var c = 0; c < game.cols; c++){
+        for(var r = 0; r < game.rows; r++){
+            drawPoint(c,r,game.grid[r][c]);
+            
+        }
+    }
+    //draw the player
+    
+    drawPoint(game.playerX, game.playerY, 2)
+
+     //draw the coin
+    
+     drawPoint(game.coinX, game.coinY, 3)
+
+     //draw the score
+     fill("red");
+     text(`Score: ${game.score}`, 10,10);
+
+}
+
+
+
+function updateGameState()
+{
+    //check is player hits wall
+    var x = game.playerX;
+    var y = game.playerY;
+    if (game.grid[y][x] == 1) game.isDead = true;
+
+    //check is player hits coin
+
+    if ((game.coinX == game.playerX && game.coinY == game.playerY)){
+        game.score = game.score + 1;
+        setRandomCoin(game);
+    }
+}
 function addNextCoin(){}
 function handleKeyboard(){}
 function checkPlayerPosition(){}
